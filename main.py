ada8146bd87c24093ae4181a3892fed98a4a5dc8
Anand <anandtrex@users.noreply.github.com>
2025-07-08 16:05:38 -0600

    Cleanup and working pruning for bptt

--- main.py
+++ main.py
@@ -57,84 +57,138 @@ def compute_influence_matrix(carry, inp):
     M = jnp.einsum('bkl,blij->bkij', J, M_prev) + bar_M
     return M, M
 
+@eqx.filter_jit
+def make_step_bptt(full_model, x, y, opt_state, optim, cell_type, prune):
+    params, static = eqx.partition(full_model, eqx.is_inexact_array)
+    trainable_params = haliax.state_dict.to_state_dict(params)
 
-def train(
-        dataset_size=10000,
-        seq_len=16,
-        batch_size=32,
-        learning_rate=3e-3,
-        steps=600,
-        hidden_size=16,
-        seed=5678,
-        weight_sparsity=0.,
-        disable_activity_sparsity=False,
-        cell_type=CellType.EGRU,
-        prune=False,
-        pruner=None,
-        use_wandb=False,
-):
-    """
-    Trains with BPTT
-    """
-    data_key, loader_key, model_key = jrandom.split(jrandom.PRNGKey(seed), 3)
+    (loss, outs), grads = compute_loss_and_grads(full_model, x, y)
+    # updates, opt_state = optim.update(grads, opt_state, eqx.filter(model, eqx.is_array))
+    grads = haliax.state_dict.to_state_dict(eqx.filter(grads, eqx.is_inexact_array))
+    updates, opt_state = optim.update(grads, opt_state, trainable_params)
+    trainable_params = eqx.apply_updates(trainable_params, updates)
+    # For jax pruner
+    if prune:
+        trainable_params = pruner.post_gradient_update(trainable_params, opt_state)
+    ## jax pruner
+    params = haliax.state_dict.from_state_dict(params, trainable_params)
+    full_model = eqx.combine(params, static)
+    time_state_sparsity = jnp.mean(outs[0] == 0., axis=2)
+    # loss, full_model, opt_state, outs, sparsity
+    return loss, full_model, opt_state, outs, (time_state_sparsity, time_state_sparsity)
 
-    xs, ys = get_data(dataset_size, seq_len, key=data_key)
-    iter_data = dataloader((xs, ys), batch_size, key=loader_key)
+
+# Important for efficiency whenever you use JAX: wrap everything into a single JIT
+# region.
+@eqx.filter_jit
+def make_step_rtrl(full_model, x, y, opt_state, optim, cell_type, prune):
+
+    params, static = eqx.partition(full_model, eqx.is_inexact_array)
+    trainable_params = haliax.state_dict.to_state_dict(params)
+    # lmm = lambda mm: mm(x), mm(x)
+
+    ## full_model: x -> pred_y, outs
+    ## For EGRU, outs = (h, c, o, i, (Jh, barMh), (Jc, barMc), (Jo, barMo), (Ji, barMi))
+    ## For eqx.GRU, outs = (h)
+    # @eqx.filter_grad
+    @eqx.filter_jit
+    def fma(params, static, xx):
+        mm = eqx.combine(params, static)
+        pred_y, outs = mm(xx)
+        return (pred_y, outs), (pred_y, outs)
+
+    # @eqx.filter_grad
+    @eqx.filter_jit
+    def jj(mm, xx):
+        params, static = eqx.partition(mm, eqx.is_array)
+        return jax.jacfwd(fma, has_aux=True)(params, static, xx)
+
+    @eqx.filter_jit
+    def ls(mm, hht, y):
+        # lg, pred_y = jax.jacfwd(lin, has_aux=True)(lambda x: jax.nn.sigmoid(mm.linear(x)), hht)
+        def _loss(lin):
+            pred_y = jax.nn.sigmoid(lin(hht))
+            l, _ = loss_fn(y, pred_y)
+            return l, l
+
+        lg, loss = jax.jacfwd(_loss, has_aux=True)(mm.linear)
+        # ipdb.set_trace()
+        return loss, (loss, lg)
+
+    ## Jouts is M at (t-1)
+    (_Jpred_y, Jouts), (_pred_y, outs) = jax.vmap(partial(jj, full_model))(x)
+    # ipdb.set_trace()
+
+    # jax.debug.print("Mean value of states: {m}", m=jnp.mean(outs[0]))
+    # jax.debug.print("Percent zeros in states: {m}", m=jnp.mean(outs[0] == 0.))
+    # jax.debug.print("Percent zeros in Ms: {m}", m=jnp.mean(jnp.isclose(Jouts[0].cell.weight_hh, 0.)))
+
+    time_state_sparsity = jnp.mean(outs[0] == 0., axis=2)
+    time_J_sparsity = jnp.mean(jnp.isclose(Jouts[0].cell.weight_hh, 0.), axis=(2, 3, 4))
 
     if cell_type in [CellType.EqxGRU]:
-        model = EqxRNN(cell_type=cell_type, in_size=2, out_size=1, hidden_size=hidden_size, key=model_key)
-    elif cell_type in [CellType.RNN]:
-        model = RNN(cell_type=cell_type, in_size=2, out_size=1, hidden_size=hidden_size, key=model_key)
+        final_state = outs[:, -1]
     else:
-        model = RNN(cell_type=cell_type, in_size=2, out_size=1, hidden_size=hidden_size, key=model_key,
-                    weight_sparsity=weight_sparsity, activity_sparse=(not disable_activity_sparsity))
+        final_state = outs[0][:, -1]
+    Jloss, (loss, lg) = jax.vmap(partial(jax.jacfwd(ls, has_aux=True, argnums=1), full_model))(final_state, y)
 
-    full_model = model
+    def calc_grad(M):
+        barC = Jloss
+        bhgrads = jnp.einsum('bh,bh...->b...', barC, M[:, -1])
+        return jnp.mean(bhgrads, axis=(0,))
 
-    # Important for efficiency whenever you use JAX: wrap everything into a single JIT
-    # region.
-    @eqx.filter_jit
-    def make_step(full_model, x, y, opt_state):
-        params, static = eqx.partition(full_model, eqx.is_inexact_array)
-        trainable_params = haliax.state_dict.to_state_dict(params)
+    if cell_type in [CellType.EqxGRU]:
+        cell_grads = jax.tree_util.tree_map(calc_grad, Jouts.cell)
+    else:
+        cell_grads = jax.tree_util.tree_map(calc_grad, Jouts[0].cell)
+    lin_grads = jax.tree_util.tree_map(partial(jnp.mean, axis=(0,)), lg)
 
-        (loss, outs), grads = compute_loss_and_grads(full_model, x, y)
-        # updates, opt_state = optim.update(grads, opt_state, eqx.filter(model, eqx.is_array))
-        grads = haliax.state_dict.to_state_dict(eqx.filter(grads, eqx.is_inexact_array))
-        updates, opt_state = optim.update(grads, opt_state, trainable_params)
-        trainable_params = eqx.apply_updates(trainable_params, updates)
-        # For jax pruner
-        if prune:
-            trainable_params = pruner.post_gradient_update(trainable_params, opt_state)
-        ## jax pruner
-        params = haliax.state_dict.from_state_dict(params, trainable_params)
-        full_model = eqx.combine(params, static)
-        return loss, full_model, opt_state, outs
+    ## Test if grads are correct (Yes they are!!)
+    loss = jnp.mean(loss)
 
-    optim = optax.adam(learning_rate)
+    # jax.debug.print("Are the hh grads correct?: {a}", a=jnp.isclose(cell_grads.weight_hh, grads_.cell.weight_hh).all())
+    # jax.debug.print("Are the lin grads correct?: {a}", a=jnp.isclose(lin_grads.weight, grads_.linear.weight).all())
+    # jax.debug.print("Grads: {a}, {b}", a=grads.weight_hh, b=grads_.cell.weight_hh)
+    # ipdb.set_trace()
+
+    # wcg = eqx.tree_at(lambda m: m.cell, trainable_params, replace=cell_grads)
+    # grads = eqx.tree_at(lambda m: m.linear, wcg, replace=lin_grads)
+    grads = haliax.state_dict.to_state_dict(eqx.filter(cell_grads, eqx.is_inexact_array), prefix="cell") | \
+        haliax.state_dict.to_state_dict(eqx.filter(lin_grads, eqx.is_inexact_array), prefix="linear")
+
+    # (loss, outs), grads_ = compute_loss_and_grads(full_model, x, y)
+    # jax.debug.print("Are the hh grads correct?: {a}", a=jnp.isclose(grads.cell.weight_hh, grads_.cell.weight_hh).all())
+    # jax.debug.print("Are the lin grads correct?: {a}", a=jnp.isclose(grads.linear.weight, grads_.linear.weight).all())
+
+    # updates, opt_state = optim.update(grads, opt_state, eqx.filter(full_model, eqx.is_array))
+    updates, opt_state = optim.update(grads, opt_state, trainable_params)
+    # ipdb.set_trace()
+    trainable_params = eqx.apply_updates(trainable_params, updates)
+    # ipdb.set_trace()
+
+    # For jax pruner
     if prune:
-        optim = pruner.wrap_optax(optim)
+        trainable_params = pruner.post_gradient_update(trainable_params, opt_state)
+    ## jax pruner
 
-    params = eqx.filter(full_model, eqx.is_inexact_array)
-    trainable_params = haliax.state_dict.to_state_dict(params)
-    opt_state = optim.init(trainable_params)
+    params = haliax.state_dict.from_state_dict(params, trainable_params)
+    full_model = eqx.combine(params, static)
 
-    for step, (x, y) in zip(range(steps), iter_data):
-        loss, full_model, opt_state, outs = make_step(full_model, x, y, opt_state)
-        # print(outs)
-        loss = loss.item()
-        print(f"step={step}, loss={loss}")
-        if prune:
-            print(jaxpruner.summarize_sparsity(full_model, only_total_sparsity=True))
+    return loss, full_model, opt_state, outs, (time_state_sparsity, time_J_sparsity)
 
-    pred_ys, outs = jax.vmap(full_model)(xs)
+    # return
 
-    num_correct = jnp.sum((pred_ys > 0.5) == ys)
-    final_accuracy = (num_correct / dataset_size).item()
-    print(f"final_accuracy={final_accuracy}")
+    # # (loss, outs), grads = compute_loss_and_grads(full_model, x, y)
+    # loss, outs = compute_loss_and_outputs(full_model, x, y)
+    # ## For now, assume only last timestep contributes to loss. TODO fix later.
+    # (new_h, new_c, new_o, new_i, jacs) = outs
+    # # (Jh, bar_Mh), (Jc, bar_Mc), (Jo, bar_Mo), (Ji, bar_Mi) = jacs
+    # updates, opt_state = optim.update(grads, opt_state)
+    # full_model = eqx.apply_updates(full_model, updates)
+    # return loss, full_model, opt_state, outs
 
-
-def train_fwd_implicit(
+def train(
+        make_step,
         dataset_size=10000,
         seq_len=17,
         batch_size=32,
@@ -192,114 +246,6 @@ def train_fwd_implicit(
     #     # jax.debug.print("{loss}", loss=loss)
     #     return loss, outs
 
-    # Important for efficiency whenever you use JAX: wrap everything into a single JIT
-    # region.
-    @eqx.filter_jit
-    def make_step(full_model, x, y, opt_state):
-
-        params, static = eqx.partition(full_model, eqx.is_inexact_array)
-        trainable_params = haliax.state_dict.to_state_dict(params)
-        # lmm = lambda mm: mm(x), mm(x)
-
-        ## full_model: x -> pred_y, outs
-        ## For EGRU, outs = (h, c, o, i, (Jh, barMh), (Jc, barMc), (Jo, barMo), (Ji, barMi))
-        ## For eqx.GRU, outs = (h)
-        # @eqx.filter_grad
-        @eqx.filter_jit
-        def fma(params, static, xx):
-            mm = eqx.combine(params, static)
-            pred_y, outs = mm(xx)
-            return (pred_y, outs), (pred_y, outs)
-
-        # @eqx.filter_grad
-        @eqx.filter_jit
-        def jj(mm, xx):
-            params, static = eqx.partition(mm, eqx.is_array)
-            return jax.jacfwd(fma, has_aux=True)(params, static, xx)
-
-        @eqx.filter_jit
-        def ls(mm, hht, y):
-            # lg, pred_y = jax.jacfwd(lin, has_aux=True)(lambda x: jax.nn.sigmoid(mm.linear(x)), hht)
-            def _loss(lin):
-                pred_y = jax.nn.sigmoid(lin(hht))
-                l, _ = loss_fn(y, pred_y)
-                return l, l
-
-            lg, loss = jax.jacfwd(_loss, has_aux=True)(mm.linear)
-            # ipdb.set_trace()
-            return loss, (loss, lg)
-
-        ## Jouts is M at (t-1)
-        (_Jpred_y, Jouts), (_pred_y, outs) = jax.vmap(partial(jj, full_model))(x)
-        # ipdb.set_trace()
-
-        # jax.debug.print("Mean value of states: {m}", m=jnp.mean(outs[0]))
-        # jax.debug.print("Percent zeros in states: {m}", m=jnp.mean(outs[0] == 0.))
-        # jax.debug.print("Percent zeros in Ms: {m}", m=jnp.mean(jnp.isclose(Jouts[0].cell.weight_hh, 0.)))
-
-        time_state_sparsity = jnp.mean(outs[0] == 0., axis=2)
-        time_J_sparsity = jnp.mean(jnp.isclose(Jouts[0].cell.weight_hh, 0.), axis=(2, 3, 4))
-
-        if cell_type in [CellType.EqxGRU]:
-            final_state = outs[:, -1]
-        else:
-            final_state = outs[0][:, -1]
-        Jloss, (loss, lg) = jax.vmap(partial(jax.jacfwd(ls, has_aux=True, argnums=1), full_model))(final_state, y)
-
-        def calc_grad(M):
-            barC = Jloss
-            bhgrads = jnp.einsum('bh,bh...->b...', barC, M[:, -1])
-            return jnp.mean(bhgrads, axis=(0,))
-
-        if cell_type in [CellType.EqxGRU]:
-            cell_grads = jax.tree_util.tree_map(calc_grad, Jouts.cell)
-        else:
-            cell_grads = jax.tree_util.tree_map(calc_grad, Jouts[0].cell)
-        lin_grads = jax.tree_util.tree_map(partial(jnp.mean, axis=(0,)), lg)
-
-        ## Test if grads are correct (Yes they are!!)
-        loss = jnp.mean(loss)
-
-        # jax.debug.print("Are the hh grads correct?: {a}", a=jnp.isclose(cell_grads.weight_hh, grads_.cell.weight_hh).all())
-        # jax.debug.print("Are the lin grads correct?: {a}", a=jnp.isclose(lin_grads.weight, grads_.linear.weight).all())
-        # jax.debug.print("Grads: {a}, {b}", a=grads.weight_hh, b=grads_.cell.weight_hh)
-        # ipdb.set_trace()
-
-        # wcg = eqx.tree_at(lambda m: m.cell, trainable_params, replace=cell_grads)
-        # grads = eqx.tree_at(lambda m: m.linear, wcg, replace=lin_grads)
-        grads = haliax.state_dict.to_state_dict(eqx.filter(cell_grads, eqx.is_inexact_array), prefix="cell") | \
-            haliax.state_dict.to_state_dict(eqx.filter(lin_grads, eqx.is_inexact_array), prefix="linear")
-
-        # (loss, outs), grads_ = compute_loss_and_grads(full_model, x, y)
-        # jax.debug.print("Are the hh grads correct?: {a}", a=jnp.isclose(grads.cell.weight_hh, grads_.cell.weight_hh).all())
-        # jax.debug.print("Are the lin grads correct?: {a}", a=jnp.isclose(grads.linear.weight, grads_.linear.weight).all())
-
-        # updates, opt_state = optim.update(grads, opt_state, eqx.filter(full_model, eqx.is_array))
-        updates, opt_state = optim.update(grads, opt_state, trainable_params)
-        # ipdb.set_trace()
-        trainable_params = eqx.apply_updates(trainable_params, updates)
-        # ipdb.set_trace()
-
-        # For jax pruner
-        if prune:
-            trainable_params = pruner.post_gradient_update(trainable_params, opt_state)
-        ## jax pruner
-
-        params = haliax.state_dict.from_state_dict(params, trainable_params)
-        full_model = eqx.combine(params, static)
-
-        return loss, full_model, opt_state, outs, (time_state_sparsity, time_J_sparsity)
-
-        # return
-
-        # # (loss, outs), grads = compute_loss_and_grads(full_model, x, y)
-        # loss, outs = compute_loss_and_outputs(full_model, x, y)
-        # ## For now, assume only last timestep contributes to loss. TODO fix later.
-        # (new_h, new_c, new_o, new_i, jacs) = outs
-        # # (Jh, bar_Mh), (Jc, bar_Mc), (Jo, bar_Mo), (Ji, bar_Mi) = jacs
-        # updates, opt_state = optim.update(grads, opt_state)
-        # full_model = eqx.apply_updates(full_model, updates)
-        # return loss, full_model, opt_state, outs
 
     optim = optax.adam(learning_rate)
     # For Jax pruner
@@ -315,7 +261,7 @@ def train_fwd_implicit(
     validation_accs = []
     cum_mean_state_density, cum_mean_M_density = 0., 0.
     for step, (x, y) in zip(range(steps), iter_data):
-        loss, full_model, opt_state, outs, sparsity = make_step(full_model, x, y, opt_state)
+        loss, full_model, opt_state, outs, sparsity = make_step(full_model, x, y, opt_state, optim, cell_type, prune)
 
         # print(outs)
         loss = loss.item()
@@ -358,7 +304,6 @@ def train_fwd_implicit(
     print(f"test_accuracy={final_accuracy}")
 
 
-
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--location', type=str, choices=['mac', 'desktop', 'jusuf', 'taurus'], default='mac')
@@ -375,23 +320,23 @@ if __name__ == '__main__':
                        prune=args.prune, cell_type=CellType.EGRU)
     # config = ml_collections.ConfigDict(config_dict)
 
-    # sparsity_config_dict = dict(
-    #     algorithm='magnitude',
-    #     update_freq=10,
-    #     update_end_step=1000,
-    #     update_start_step=200,
-    #     sparsity=0.95,
-    #     dist_type='erk',
-    # )
-    # config.sparsity_config = ml_collections.ConfigDict(sparsity_config_dict)
     if args.prune:
-        sparsity_distribution = partial(jaxpruner.sparsity_distributions.uniform, sparsity=0.8)
-        pruner = jaxpruner.MagnitudePruning(sparsity_distribution_fn=sparsity_distribution)
-        # sparsity_updater = jaxpruner.create_updater_from_config(config.sparsity_config)
+        sparsity_config_dict = dict(
+            algorithm='magnitude',
+            update_freq=10,
+            update_end_step=1000,
+            update_start_step=200,
+            sparsity=0.95,
+            dist_type='erk',
+        )
+        sparsity_config = ml_collections.ConfigDict(sparsity_config_dict)
+        # sparsity_distribution = partial(jaxpruner.sparsity_distributions.uniform, sparsity=0.8)
+        # pruner = jaxpruner.MagnitudePruning(sparsity_distribution_fn=sparsity_distribution)
+        pruner = jaxpruner.create_updater_from_config(sparsity_config)
 
     if args.method == 'rtrl':
-        train_fwd_implicit(pruner=pruner, **config_dict)  # All right, let's run the code.
+        train(make_step=make_step_rtrl, pruner=pruner, **config_dict)  # All right, let's run the code.
     elif args.method == 'bptt':
-        train(pruner=pruner, **config_dict)
+        train(make_step=make_step_bptt, pruner=pruner, **config_dict)
     else:
         raise RuntimeError(f"Unknown method {args.method}")
